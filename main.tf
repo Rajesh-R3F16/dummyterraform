d768e9a24d22e1974496efd4b190a8b7d1cc98c6
Rajesh-R3F16 <rajesha589@outlook.com>
2025-05-14 21:13:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,27 +2,11 @@ provider "aws" {
 region = "us-east-1"
 }
 
-terraform {
-  backend "remote" {
-    organization = "raham"
-
-    workspaces {
-      name = "dummyterraform"
-    }
-  }
-}
-
-
-resource "aws_instance" "three" {
-  count         =  3
-  ami           = "ami-00b8917ae86a424c9"
-  instance_type = "t2.medium"
+resource "aws_instance" "one" {
+  ami           = "ami-085386e29e44dacd79"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "mumbai-server"
+    Name = "test-server"
   }
-}
-
-resource "aws_s3_bucket" "abcd" {
-bucket = "devopsherahamshaik0099889977hcp"
 }
