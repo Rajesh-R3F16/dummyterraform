13bdf7fdb71253dcb9c732d279b9ee5ff0aafa8d
Rajesh-R3F16 <rajesha589@outlook.com>
2025-05-14 22:10:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-  ami           = "ami-085386e29e44dacd79"
+  ami           = "ami-085386e29e44dacd7"
   instance_type = "t2.micro"
 
   tags = {
